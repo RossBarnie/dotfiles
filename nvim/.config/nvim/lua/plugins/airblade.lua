6221762c00efa895b5868fd433a0e3250ce89380
Ross Barnie <r.barnie@gmail.com>
2023-09-12 11:22:21 +0100

    Fix vim-rooter not following

--- nvim/.config/nvim/lua/plugins/airblade.lua
+++ nvim/.config/nvim/lua/plugins/airblade.lua
@@ -1,9 +1,5 @@
+vim.g.rooter_cd_cmd = 'lcd'
 return {
   'airblade/vim-gitgutter', -- git diff as signs in sign column
-  {
-    'airblade/vim-rooter', -- git diff as signs in sign column
-    config = function()
-      vim.g.rooter_cd_cmd = 'lcd'
-    end
-  }
+  'airblade/vim-rooter', -- git diff as signs in sign column
 }
