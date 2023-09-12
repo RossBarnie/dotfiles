a233c75431fb751aac14d498833d946754fb16a6
Ross Barnie <r.barnie@gmail.com>
2023-09-12 11:23:12 +0100

    Add initial LSP config

---
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,46 @@
+return {
+  {
+    'williamboman/mason.nvim',
+    config = function()
+      require('mason').setup{}
+    end
+  },
+  {
+    'neovim/nvim-lspconfig',
+    keys = {
+      { '<space>e', vim.diagnostic.open_float },
+      { '[d', vim.diagnostic.goto_prev },
+      { ']d', vim.diagnostic.goto_next },
+      { '<space>q', vim.diagnostic.setloclist },
+    },
+    config = function()
+      local lsp = require('lspconfig')
+      lsp.rust_analyzer.setup {
+        settings = {
+          ['rust-analyzer'] = {},
+        }
+      }
+      lsp.quick_lint_js.setup {
+        handlers = {
+          ['textDocument/publishDiagnostics'] = vim.lsp.with(
+            vim.lsp.diagnostic.on_publish_diagnostics, {
+              update_in_insert = true
+            }
+          )
+        }
+      }
+      lsp.solargraph.setup {}
+      lsp.marksman.setup {}
+      lsp.yamlls.setup {
+        settings = {
+          yaml = {
+            schemastore = {
+              enable = false
+            },
+            schemas = require('schemastore').yaml.schemas(),
+          }
+        }
+      }
+    end
+  }
+}
