81069049f09572d144336d3662a7d8567ef31e72
Ross Barnie <r.barnie@gmail.com>
2023-09-13 14:31:56 +0100

    Fix nvim-tree not following when changing projects

--- nvim/.config/nvim/lua/plugins/airblade.lua
+++ nvim/.config/nvim/lua/plugins/airblade.lua
@@ -1,4 +1,3 @@
-vim.g.rooter_cd_cmd = 'lcd'
 return {
   'airblade/vim-gitgutter', -- git diff as signs in sign column
   'airblade/vim-rooter', -- change root directory automatically when changing files
