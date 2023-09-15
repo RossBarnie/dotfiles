1e75e0ba2bc944a823831c07e277e507b8907261
Ross Barnie <r.barnie@gmail.com>
2023-09-15 10:59:50 +0100

    Replace nvim-tree with neo-tree
    Couldn't resolve problem of nvim-tree not following current working
    directory of buffer but neo-tree works out of the box.

---
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,25 @@
+local k = function(lhs, rhs)
+  vim.keymap.set('n', lhs, rhs)
+end
+
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      require("neo-tree").setup {
+        filesystem = {
+          filtered_items = {
+            hide_dotfiles = false
+          }
+        }
+      }
+      k('<C-n>', ':Neotree toggle<CR>')
+    end
+  }
+}
