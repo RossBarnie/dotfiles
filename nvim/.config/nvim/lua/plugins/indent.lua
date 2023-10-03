9cd5ab22ed3d6b0e5a8714040b50b601d45a2643
Ross Barnie <r.barnie@gmail.com>
2023-10-03 11:12:48 +0100

    Configure indent-blankline for v3

--- nvim/.config/nvim/lua/plugins/indent.lua
+++ nvim/.config/nvim/lua/plugins/indent.lua
@@ -1,4 +1,19 @@
 return {
-  'lukas-reineke/indent-blankline.nvim',
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
+    config = function()
+      require('ibl').setup {}
+      local hooks = require('ibl.hooks');
+      hooks.register(
+        hooks.type.WHITESPACE,
+        hooks.builtin.hide_first_space_indent_level
+      )
+      hooks.register(
+        hooks.type.WHITESPACE,
+        hooks.builtin.hide_first_tab_indent_level
+      )
+    end
+  }
 }
 
