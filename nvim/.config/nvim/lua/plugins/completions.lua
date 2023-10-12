f75bd764cdac853a8fc35359be90a299eaf6f936
Ross Barnie <r.barnie@gmail.com>
2023-10-12 11:01:09 +0100

    Remove conflicting keymaps

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -3,7 +3,13 @@ return {
     'ms-jpq/coq_nvim',
     branch = 'coq',
     init = function()
-      vim.g.coq_settings = { auto_start = 'shut-up' }
+      vim.g.coq_settings = {
+        auto_start = 'shut-up',
+        keymap = {
+          bigger_preview = '',
+          jump_to_mark = ''
+        }
+      }
     end
   }
 }
