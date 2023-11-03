9d3d1a688820543390e133245609581ffcd5561a
Ross Barnie <r.barnie@gmail.com>
2023-11-03 11:17:44 +0000

    Fix Glow not editor command error

--- nvim/.config/nvim/lua/plugins/glow.lua
+++ nvim/.config/nvim/lua/plugins/glow.lua
@@ -1,6 +1,13 @@
+local k = function(lhs, rhs, desc)
+  vim.keymap.set('n', lhs, rhs, { desc = desc })
+end
+
 return {
-  "ellisonleao/glow.nvim",
-  keys = {
-    { '<Leader>md', ':Glow<CR>' }
+  {
+    "ellisonleao/glow.nvim",
+    config = function()
+      require('glow').setup()
+      k('<Leader>md', ':Glow<CR>')
+    end
   }
 }
