d2ae798ea5a1a4ecfd76d7c690f16580b52be0e9
Ross Barnie <r.barnie@gmail.com>
2024-04-26 12:06:29 +0100

    Add LSP progress to lualine
    Useful to show LSP activity.

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,14 +1,28 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  config = function()
-    require("lualine").setup {
-      options = {
-        icons_enabled = false,
-        theme = 'nordic'
-      },
-      extensions = {
-        'fugitive'
+  {
+    'linrongbin16/lsp-progress.nvim',
+    config = function()
+      require('lsp-progress').setup()
+    end
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+      'linrongbin16/lsp-progress.nvim'
+    },
+    config = function()
+      require("lualine").setup {
+        sections = {
+          lualine_c = { require('lsp-progress').progress }
+        },
+        options = {
+          icons_enabled = false,
+          theme = 'nordic'
+        },
+        extensions = {
+          'fugitive'
+        }
       }
-    }
-  end
+    end
+  }
 }
