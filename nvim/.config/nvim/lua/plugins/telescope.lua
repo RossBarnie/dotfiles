411ecec2484617c6253cf296ac7685a9dec61865
Ross Barnie <r.barnie@gmail.com>
2024-04-26 11:44:13 +0100

    Make find mappings consistent
    While this makes the find commands slightly less convenient (eg the
    change from `<leader>f` to `<leader>ff`, it makes the mappings far more
    consistent and in line with the language of basic vim commands. For
    example in the past I had to remember the specific key bindings for each
    use-case, now I just have to remember a single key for the kind of
    search I'm doing, because everything is being searched with a
    "<Leader>f(ind)" prefix.

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -5,10 +5,13 @@ return {
   'nvim-telescope/telescope.nvim',
   config = function()
     local telescope = require("telescope")
-    k('<Leader><CR>', ':Telescope repo list<CR>')
-    k('<Leader>d', ':Telescope lsp_definitions<CR>')
-    k('<Leader>f', ':Telescope live_grep<CR>')
-    k('<C-p>', ':Telescope find_files<CR>')
+    k('<Leader>fp', ':Telescope repo list<CR>') -- find project
+    k('<Leader>fd', ':Telescope lsp_definitions<CR>') -- find definition
+    k('<Leader>fs', ':Telescope live_grep<CR>') -- find string
+    k('<Leader>fb', ':Telescope buffers<CR>') -- find buffer
+    k('<Leader>ff', ':Telescope find_files<CR>') -- find files
+    k('<Leader>fh', ':Telescope help_tags<CR>') -- find help (vim)
+    k('<Leader>fm', ':Telescope man_pages<CR>') -- find help (man)
     telescope.setup {
       defaults = {
         layout_strategy = 'vertical'
