53fe72f0467e75c3d3f325928a12026b1a10bfcd
Ross Barnie <r.barnie@gmail.com>
2025-04-28 10:32:34 +0100

    feat(nvim)!: Update nvim to v0.10
    Breaking change in the sense that the lspconfig plugin has been updated
    to a version that does not support v0.9.

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -17,7 +17,7 @@ return {
     dependencies = {
       'b0o/schemastore.nvim'
     },
-    version = '1.8.x', -- See https://github.com/neovim/nvim-lspconfig/issues/3693, Neovim 0.10 required
+    version = '*',
     config = function()
       local lsp = require('lspconfig')
       local capabilities = vim.tbl_deep_extend("force",
