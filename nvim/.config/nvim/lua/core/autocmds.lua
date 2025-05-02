76fecf454ed2259aa21e5ac1d9ce9355d528fc77
Ross Barnie <r.barnie@gmail.com>
2025-05-02 15:56:44 +0100

    fix(docs): Add documentation for autocmds

--- nvim/.config/nvim/lua/core/autocmds.lua
+++ nvim/.config/nvim/lua/core/autocmds.lua
@@ -1,8 +1,13 @@
+-- Set up augroup for cleanup
 vim.api.nvim_create_augroup("vimrc", { clear = true })
+
+-- Resize nvim on terminal resize
 vim.api.nvim_create_autocmd({"VimResized"}, {
   pattern = { "*" },
   command = ":wincmd =",
 })
+
+-- Format using LSP on save
 vim.api.nvim_create_autocmd({"BufWritePre"}, {
   pattern = { "*" },
   callback = vim.lsp.buf.format,
