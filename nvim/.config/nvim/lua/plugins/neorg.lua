696db3091b60352682f6385acc54731f5d5451a1
Ross Barnie <r.barnie@gmail.com>
2025-05-02 14:57:21 +0100

    fix(neorg)!: Set notes project path
    Breaking change in that the path has changed from any local setting.

--- nvim/.config/nvim/lua/plugins/neorg.lua
+++ nvim/.config/nvim/lua/plugins/neorg.lua
@@ -2,5 +2,19 @@ return {
   "nvim-neorg/neorg",
   lazy = false,
   version = "*",
-  config = true
+  config = true,
+  opts = {
+    load = {
+      ["core.defaults"] = {},
+      ["core.dirman"] = {
+        config = {
+          workspaces = {
+            notes = "~/projects/personal/notes",
+          },
+          default_workspace = "notes",
+        }
+      },
+      ["core.concealer"] = {}
+    }
+  }
 }
