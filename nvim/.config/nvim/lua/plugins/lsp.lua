456e450e54aa4fdefd65a10c54a01e8e0d8ef43b
Ross Barnie <r.barnie@gmail.com>
2025-05-05 11:47:25 +0100

    fix(nvim): Diagnostic jumps not opening floats
    Default keymap from lspconfig doesn't open the float.

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -140,10 +140,17 @@ return {
         callback = function(ev)
           -- Enable completion triggered by <c-x><c-o>
           vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+          local jump = function(count)
+            return function()
+              vim.diagnostic.jump({ count = count, float = true })
+            end
+          end
 
           -- Buffer local mappings.
           -- See `:help vim.lsp.*` for documentation on any of the below functions
           local opts = { buffer = ev.buf }
+          vim.keymap.set('n', ']d', jump(1), opts)
+          vim.keymap.set('n', '[d', jump(-1), opts)
           vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
           vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
           vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
