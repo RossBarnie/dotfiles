380a8bd5e185b336d3f500701342623ca19ae559
Ross Barnie <r.barnie@gmail.com>
2025-07-18 14:03:00 +0100

    chore(nvim): Remove unnecessary commented mapping
    Reloading nvim configs is not recommended and frankly this hasn't worked
    for a very long time, so just removing.

--- nvim/.config/nvim/lua/core/mappings.lua
+++ nvim/.config/nvim/lua/core/mappings.lua
@@ -17,8 +17,6 @@ k.set('n', '<Leader>i', 'mmgg=G`m<CR>')
 
 -- edit init.vim config (less useful since the modularisation, what should this do instead?
 -- k.set('n', '<Leader>ev', ':tabe ~/.config/nvim/init.lua<CR>')
--- reload init.lua
-k.set('n', '<Leader>rv', ':so ~/.config/nvim/init.lua<CR>:echom \'init.lua reloaded\'<CR>')
 
 -- remove highlights
 k.set('n', '<Leader>h', ':noh<CR>')
