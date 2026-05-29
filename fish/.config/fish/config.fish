189e297ebfbbb4210b7f9cbe5fe78e60cb9a96f1
Ross Barnie <r.barnie@gmail.com>
2026-05-29 16:38:58 +0100

    feat: Add fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,16 @@
+if test -f /usr/share/cachyos-fish-config/cachyos-config.fish
+  source /usr/share/cachyos-fish-config/cachyos-config.fish
+end
+
+# overwrite greeting
+function fish_greeting
+end
+
+set -x XDG_CONFIG_HOME $HOME/.config
+set -x XDG_DATA_HOME $HOME/.local/share
+set -x RIPGREP_CONFIG_PATH $XDG_CONFIG_HOME/rg/.ripgreprc
+
+set -x EDITOR nvim
+set -x VISUAL nvim
+
+zoxide init fish | source
